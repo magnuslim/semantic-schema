bf7cf3f7f6aeed493672e4bfc98c7434cfccb0ee
MagnusLin <magnuslin@vanchu.net>
2018-01-11 19:27:15 +0800

    use desc instead of description in describer.

--- src/describer/base.js
+++ src/describer/base.js
@@ -4,12 +4,9 @@ module.exports = class {
     constructor() {
         this._schema = {};
     }
-    description(desc) {
+    desc(desc) {
         this._schema.description = desc;
         return this;
-    }
-    desc(desc) {
-        return this.description(desc);
     }
     enum(...enumArr) {
         this._schema.enum = enumArr;
